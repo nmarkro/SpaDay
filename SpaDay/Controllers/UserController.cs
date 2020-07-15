021f06018330cde46507eb4c750ef9d5c2e11c17
nmarkro <nmarkro@gmail.com>
2020-07-15 16:48:09 -0500

    finish studio

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.username = "";
+            ViewBag.email = "";
+            ViewBag.error = false;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.username = newUser.Username;
+            ViewBag.email = newUser.Email;
+            if (newUser.Password.Equals(verify))
+            {
+                return View("Index");
+            } else
+            {
+                ViewBag.error = true;
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        [FromForm(Name ="username")]
+        public string Username { get; set; }
+        [FromForm(Name = "email")]
+        public string Email { get; set; }
+        [FromForm(Name = "password")]
+        public string Password { get; set; }
+
+
+        public User() { }
+
+        public User(string username, string email, string password)
+        {
+            this.Username = username;
+            this.Email = email;
+            this.Password = password;
+        }
+    }
+}
